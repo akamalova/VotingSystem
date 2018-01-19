c619671c57d7edcc713a1fba81ef2200e7a8fc0c
Айгуль Камалова <camomile.2010@mail.ru>
2018-01-19 18:03:56 +0300

    ExceptionInfoHandler added

--- src/main/java/ru/testAssignment/voting/util/ValidationUtil.java
+++ src/main/java/ru/testAssignment/voting/util/ValidationUtil.java
@@ -1,12 +1,16 @@
 package ru.testAssignment.voting.util;
 
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import ru.testAssignment.voting.HasId;
 import ru.testAssignment.voting.model.AbstractBaseEntity;
 import ru.testAssignment.voting.util.exception.NotFoundException;
 import ru.testAssignment.voting.util.exception.TimesUpException;
 
 import java.time.LocalTime;
+import java.util.StringJoiner;
 
 public class ValidationUtil {
 
@@ -54,6 +58,30 @@ public class ValidationUtil {
             throw new IllegalArgumentException(bean + " must be with id=" + id);
         }
     }
+
+    //  http://stackoverflow.com/a/28565320/548473
+    public static Throwable getRootCause(Throwable t) {
+        Throwable result = t;
+        Throwable cause;
+
+        while (null != (cause = result.getCause()) && (result != cause)) {
+            result = cause;
+        }
+        return result;
+    }
+
+    /*public static ResponseEntity<String> getErrorResponse(BindingResult result) {
+        StringJoiner joiner = new StringJoiner("<br>");
+        result.getFieldErrors().forEach(
+                fe -> {
+                    String msg = fe.getDefaultMessage();
+                    if (!msg.startsWith(fe.getField())) {
+                        msg = fe.getField() + ' ' + msg;
+                    }
+                    joiner.add(msg);
+                });
+        return new ResponseEntity<>(joiner.toString(), HttpStatus.UNPROCESSABLE_ENTITY);
+    }*/
 
     public static void checkTime(LocalTime time) {
         if (!time.isBefore(isTest? TIME_LIMIT_MAX : TIME_LIMIT)) throw new TimesUpException();
--- src/main/java/ru/testAssignment/voting/util/exception/TimesUpException.java
+++ src/main/java/ru/testAssignment/voting/util/exception/TimesUpException.java
@@ -1,10 +1,6 @@
 package ru.testAssignment.voting.util.exception;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
-//  http://stackoverflow.com/a/22358422/548473
-@ResponseStatus(value = HttpStatus.FORBIDDEN, reason = "Late to voted")  // 403
 public class TimesUpException extends RuntimeException  {
     public TimesUpException() {
         super("Late to voted today!");
